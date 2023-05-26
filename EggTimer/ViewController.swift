d1cbfb995365bc0ee187be661de89f34349d1683
Md Tasib Haider <hridoyzylex@gmail.com>
2023-05-26 16:49:31 +0600

    Trying to add delay to create a Timer.

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,22 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let softTime = 5
-    let mediumTime = 7
-    let hardTime = 12
+//    let softTime = 5
+//    let mediumTime = 7
+//    let hardTime = 12
+    
+    let eggTime = ["Soft" : 5, "Medium" : 7, "Hard" : 12]
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
             
-        let hardness = sender.currentTitle
+        let hardness = sender.currentTitle!
+        var secondsRemaining = 30
         
-        if (hardness == "Soft"){
-            print(softTime)
-        }else if(hardness == "Medium"){
-                    print(mediumTime)
-        }else if(hardness == "Hard"){
-            print(hardTime)
+        print(eggTime[hardness]!)
+//        if (hardness == "Soft"){
+//            print(eggTime["Soft"]!)
+//        }else if(hardness == "Medium"){
+//                    print(eggTime["Medium"]!)
+//        }else if(hardness == "Hard"){
+//            print(eggTime["Hard"]!)
+//        }
+        
+        
+        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (Timer) in
+            if self.secondsRemaining > 0 {
+                print ("\(self.secondsRemaining) seconds")
+                self.secondsRemaining -= 1
+            } else {
+                Timer.invalidate()
+            }
         }
-        
-    }
-    
 }
