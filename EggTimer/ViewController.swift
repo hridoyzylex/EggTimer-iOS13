b5b1fba0f35a93f92b231a6e8bd934d135a458c8
Md Tasib Haider <hridoyzylex@gmail.com>
2023-05-29 10:59:46 +0600

    Added timer to show log in the console and a message in the Screen.

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,33 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
     
-//    let softTime = 5
-//    let mediumTime = 7
-//    let hardTime = 12
+    @IBOutlet weak var titleLabel: UILabel!
     
-    let eggTime = ["Soft" : 5, "Medium" : 7, "Hard" : 12]
+    //    let softTime = 5
+    //    let mediumTime = 7
+    //    let hardTime = 12
+    
+    let eggTimes = ["Soft" : 3, "Medium" : 4, "Hard" : 7]
+    var secondsRemaining = 60
+    var timer = Timer()
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        timer.invalidate()
+        
+        let hardness = sender.currentTitle! //Soft, Medium, Hard
             
-        let hardness = sender.currentTitle!
-        var secondsRemaining = 30
+        secondsRemaining = eggTimes[hardness]!
         
-        print(eggTime[hardness]!)
-//        if (hardness == "Soft"){
-//            print(eggTime["Soft"]!)
-//        }else if(hardness == "Medium"){
-//                    print(eggTime["Medium"]!)
-//        }else if(hardness == "Hard"){
-//            print(eggTime["Hard"]!)
-//        }
+        //        print(eggTime[hardness]!)
+        //        if (hardness == "Soft"){
+        //            print(eggTime["Soft"]!)
+        //        }else if(hardness == "Medium"){
+        //                    print(eggTime["Medium"]!)
+        //        }else if(hardness == "Hard"){
+        //            print(eggTime["Hard"]!)
+        //        }
         
-        
-        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (Timer) in
-            if self.secondsRemaining > 0 {
-                print ("\(self.secondsRemaining) seconds")
-                self.secondsRemaining -= 1
-            } else {
-                Timer.invalidate()
-            }
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateTimer(){
+        if secondsRemaining > 0{
+            print("\(secondsRemaining) seconds.")
+            secondsRemaining -= 1
         }
+        else {
+            timer.invalidate()
+            titleLabel.text = "DONE!"
+        }
+    }
 }
+
